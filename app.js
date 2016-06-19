583d2ec74ee040527b4b0276e4b512114378d7cc
Indigo253931 <indigo253931@gmail.com>
2016-06-19 16:48:44 -0600

    tictactoe

---
+++ app.js
@@ -0,0 +1,70 @@
+
+var TicTacToeGame = TicTacToeGame(); {
+
+var AskUser = prompt()
+
+
+var Markerx = document.createElement("Markerx"); {
+return Markerx;
+}
+
+function selectX () {
+$document.ready
+}
+
+function selectO () {
+	
+}
+
+function currentSquare () {
+
+}
+
+var playerScore = document.createElement("playerScore");
+var button = document.createElement("Button");
+
+
+document.getElementById("").innerHTML = Markerx;
+
+var MarkerO = document.createElement("MarkerO"); {
+return MarkerO;
+}
+
+ var playerChoice = "X";
+		var playerScore = 0;
+		var computerChoice = "O";
+		var computerScore = 0;
+
+		var playersTurn = true;
+	document.getElementById("test").addEventListener("click", function (event) {
+		event.target.innerHTML = "click count:"
+	}
+
+topLeft.onclick=turnBlue(this, 'blue'); {}
+topCenter.onclick=turnGreen(this, 'green'); {}
+Top_Right_Square.onclick=turnPink(this, '#2EAAB3'); {}
+Middle_Left_Square.onclick=turnPurple(this, 'purple'); {}
+Middle_Center_Square.onclick=turnRed(this, 'red'); {}
+Middle_Right_Square.onclick=turnYellow(this, 'yellow'); {}
+Bottom_Left_Square.onclick=turnOrange(this, 'orange'); {}
+Bottom_Center_Square.onclick=turnAqua(this, 'aqua'); {}
+Bottom_Right_Square.onclick=turnMagenta(this, 'magenta'); {}
+
+
+function draw() {
+  $(".instructions").append("<div>draw</div>");
+  currentSquare().html('<div class="dot"></div>')
+  currentSquare().find('.dot').css('background', kyrel.color).addClass('dot-'+kyrel.color);
+}
+
+
+
+$('.playerScore').html(playerScore);		    	
+		    	resetBoard();
+		    };
+		    if (checkTie() == true) {
+		    	resetBoard();
+		    };
+
+
+}
